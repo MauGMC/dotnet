f54b6115e10227363514c1779876c7733b4fe39e
MauGMC <mmauriciogiovani@gmail.com>
2025-03-20 23:07:56 -0600

    Entrega parcial

---
+++ 1erProyecto/Pages/Estadisticas.cshtml.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+public class EstadisticasModel : PageModel
+{
+
+    public void OnGet()
+    {
+        
+    }
+
+
+}
---
+++ 1erProyecto/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml;
+using System.Xml.Schema;
+using System.Text;
+using System.IO;
+
+namespace _1erProyecto.Pages;
+
+public class IndexModel : PageModel
+{
+    
+    public DataTable Alumnos=new DataTable();
+    
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        ObtenerDatos();
+    }
+    private void ObtenerDatos()
+    {
+        try
+        {
+            DataSet dst = new DataSet();
+            dst.ReadXmlSchema("bin/Debug/net7.0/datos.xml");
+            dst.ReadXml("bin/Debug/net7.0/datos.xml");
+            Alumnos = dst.Tables[0];
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
